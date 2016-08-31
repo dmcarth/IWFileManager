4a151c5deded3bdcc192023279df766d615d4732
Dylan McArthur <dylanmcarthur@alex.local>
2016-08-31 14:11:18 -0700

    Initial Commit

---
+++ IWFileManagerTests/IWFileManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  IWFileManagerTests.swift
+//  IWFileManagerTests
+//
+//  Created by Dylan McArthur on 8/31/16.
+//  Copyright © 2016 Dylan McArthur. All rights reserved.
+//
+
+import XCTest
+@testable import IWFileManager
+
+class IWFileManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
