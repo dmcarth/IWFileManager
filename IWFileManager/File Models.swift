f0f9286a752d3347bcb09cea5015f54faab584ef
Dylan McArthur <dmcarth@users.noreply.github.com>
2016-11-02 01:35:13 -0700

    Created tree structure

--- IWFileManager/File Models.swift
+++ IWFileManager/File Models.swift
@@ -10,29 +10,80 @@ public enum FileNodeType {
 	case file, folder
 }
 
-open class FileNode: CustomStringConvertible {
+public class DirectoryTreeNode {
 	
-	open var name = ""
+	internal var parent: DirectoryTreeNode?
+	internal var children = [DirectoryTreeNode]()
 	
-	open var type: FileNodeType = .file
-	
-	open var level = 0
-	
-	open var url: URL
-	
-	public init(url: URL, type: FileNodeType) {
-		self.url = url
-		self.name = url.lastPathComponent
-		self.type = type
+	public var name: String
+	public var url: URL
+	public var type = FileNodeType.file
+	public var isExpanded = false
+	public var flatChildren: [DirectoryTreeNode] {
+		if needsTraversal {
+			euler = eulerTrail(for: self)
+		}
+		needsTraversal = false
+		return euler
 	}
 	
-	open var description: String {
-		var ticks = ""
-		for _ in 0..<level {
-			ticks += "- "
+	fileprivate var needsTraversal = true
+	fileprivate var euler = [DirectoryTreeNode]()
+	
+	init(url: URL) {
+		self.url = url
+		self.name = url.lastPathComponent
+	}
+	
+	func addChild(_ child: DirectoryTreeNode) {
+		self.children.append(child)
+		child.parent = self
+	}
+	
+	func eulerTrail(for node: DirectoryTreeNode) -> [DirectoryTreeNode] {
+		var buff = [DirectoryTreeNode]()
+		
+		for child in children {
+			buff.append(child)
+			if child.isExpanded {
+				buff.append(contentsOf: eulerTrail(for: child))
+			}
 		}
 		
-		return ticks + name + " (\(type)) "
+		return buff
+	}
+	
+}
+
+extension DirectoryTreeNode: Collection {
+	
+	public typealias Index = Int
+	
+	public var startIndex: Int {
+		return 0
+	}
+	
+	public var endIndex: Int {
+		return children.count
+	}
+	
+	public func index(after i: Int) -> Int {
+		return i + 1
+	}
+	
+	public subscript(_ i: Int) -> DirectoryTreeNode {
+		return children[i]
+	}
+}
+
+extension DirectoryTreeNode: CustomStringConvertible {
+	
+	public var description: String {
+		return name
+	}
+	
+	func debugString() -> String {
+		return name +  " { " + children.map { $0.description }.joined(separator: "\n") + " } "
 	}
 	
 }
